aef9bf2d1c80649949dd494bec6750316617d442
Fraxoo <fraxo.officiel@gmail.com>
2025-10-08 15:13:13 +0200

    lien cliquable

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -4,13 +4,15 @@
 movies.forEach(movie => {
     const template = document.querySelector("#movie-list");
     const cloneTemplate = template.content.cloneNode(true);
+    const lien = cloneTemplate.querySelector("a");
     const image = cloneTemplate.querySelector("img");
     const name = cloneTemplate.querySelector("#name");
     const diffusion = cloneTemplate.querySelector("#diffusion");
 
-    image.setAttribute("src","https://picsum.photos/200/300")
+    lien.setAttribute("href", `http://localhost:8080/movie/show/${movie.id}`);
+    image.setAttribute("src", "https://picsum.photos/200/300")
     name.textContent = movie.name;
-    diffusion.textContent = "test";
+    diffusion.textContent = "diffusion";
 
     const movieList = document.querySelector(".movie-list");
     movieList.appendChild(cloneTemplate);
--- app/views/home.php
+++ app/views/home.php
@@ -3,11 +3,13 @@
         <h1>Film et diffusion</h1>
         <div class="movie-list">
             <template id="movie-list">
-                <div class="single-movie">
-                    <img src="">
-                    <p id="name">name</p>
-                    <p id="diffusion">diffusion</p>
-                </div>
+                <a href="http://localhost:8080/movie/show/0">
+                    <div class="single-movie">
+                        <img src="">
+                        <p id="name">name</p>
+                        <p id="diffusion">diffusion</p>
+                    </div>
+                </a>
             </template>
         </div>
     </div>
