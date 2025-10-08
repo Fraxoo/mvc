ca4a236ff50cf0fe0c0bee1afa19861a6de6766a
Fraxoo <fraxo.officiel@gmail.com>
2025-10-08 13:23:08 +0200

    layout

--- app/views/layout.php
+++ app/views/layout.php
@@ -1,5 +1,38 @@
 <header>
-    <div class="home">
-        <a href="http://localhost:8080/">Allociné</a>
+    <div class="home-background">
+        <div class="width">
+            <div class="home">
+                <a href="http://localhost:8080/"><img src="https://fr.web.img6.acsta.net/commons/v9/allocine/logo-custom/logo-ssm25-white.svg"> </a>
+                <form id="search_form" method="post">
+                    <div class="search">
+                        <input type="search" placeholder="Rechercher un film, une série, une star..." name="search">
+                        <button class="search-button"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </form>
+                <div class="hidden">
+                    <p class="hidden"></p>
+                </div>
+            </div>
+        </div>
+        <div class="width">
+            <div class="nav-bar">
+                <div class="left-nav-bar">
+                    <a class="open" href="http://localhost:8080/">Film</a>
+                    <a class="hover" href="http://localhost:8080/">Diffusion</a>
+                    <a class="hover" href="http://localhost:8080/">Ajout</a>
+                </div>
+                <a class="hover" href="http://localhost:8080/">Mon compte</a>
+            </div>
+        </div>
+    </div>
+    <div class="below-header-background bold">
+        <div class="width">
+            <div class="below-header-nav">
+                <a href="http://localhost:8080/">News cinéma</a>
+                <a href="http://localhost:8080/">News séries</a>
+                <a href="http://localhost:8080/">Diaporamas</a>
+            </div>
+        </div>
+    </div>
     </div>
 </header>
